61a4734e1cb1a7ede38cb637a5fbcfd3d269b68b
“saschakala” <mxsaschakala@gmail.com>
2020-11-27 19:39:03 -0500

    commit tree 25eb5fb859abe65f2619084acbd4eab860371399

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+function submitData(name, email) {
+
+    const body = document.querySelector('body')
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    }; 
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        const paragraph = document.createElement('p');
+        paragraph.innerHTML += `${object.name} ${object.email} ${object.id}`; 
+        body.appendChild(paragraph);
+      })
+      .catch(function(error){
+        const paragraph = document.createElement('p');
+        paragraph.innerHTML += `${error.message}`;
+        body.appendChild(paragraph);
+      })
+
+
+
+}
